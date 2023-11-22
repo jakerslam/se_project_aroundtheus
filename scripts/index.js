02f78d91939f20e099f50a86026eb2651fe2aa00
Jay Haslam <jakerslam@gmail.com>
2023-11-22 14:50:00 -0700

    create index.js

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+
+
+let cardData1 = {
+    title: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+let cardData2 = {
+    title: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+
+let cardData3 = {
+    title: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+
+let cardData4 = {
+    title: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+
+let cardData5 = {
+    title: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+
+let cardData6 = {
+    title: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
+
+let initialCards = [cardData1,cardData2,cardData3,cardData4,cardData5,cardData6];
+
+console.log(initialCards);
