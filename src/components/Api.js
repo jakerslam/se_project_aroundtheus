973f9808abc2ea419474f356de7c427b9ed26d26
Jay Haslam <jakerslam@gmail.com>
2024-09-18 16:44:46 -0600

    condense api.js code

--- src/components/Api.js
+++ src/components/Api.js
@@ -12,123 +12,78 @@ export default class Api {
     return Promise.reject(`Error: ${res.status}`); 
   }
 
-  getInitialProfile() {
-    return fetch(`${this._baseUrl}/users/me`, {
-      method: "GET",
-      headers: this._headers,
-    })
-    .then((res)=> {
-      return this._proccessResponse(res);
-    })  
+  _request(url, options) {
+    return fetch(url, options)
+    .then(this._proccessResponse)
     .catch((err) => {
       console.error(err);
+    })
+  }
+
+  getInitialProfile() {
+    this._request(`${this._baseUrl}/users/me`,{
+      method: "GET",
+      headers: this._headers,
     });
   }
 
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
+    this._request(`${this._baseUrl}/cards`, {
       method: "GET",
       headers: this._headers,
-    })
-    .then((res)=> { 
-      return this._proccessResponse(res);
-    })  
-    .catch((err) => {
-      console.error(err);
     });
   }
 
   postProfileItem(item) {
-    return fetch(`${this._baseUrl}/users/me`, {
+    this._request(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         name: item.name,
         about: item.about,
       })
-    })
-    .then((res)=> {
-      return this._proccessResponse(res);
-    })  
-    .catch((err) => {
-      console.error(err);
     });
   }
 
   postCard(cardData) {
-    return fetch(`${this._baseUrl}/cards`, {
+    this._request(`${this._baseUrl}/cards`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name: cardData.name,
         link: cardData.link,
       })
-    })
-    .then((res)=> {
-      return this._proccessResponse(res);
-    })  
-    .catch((err) => {
-      console.error(err);
     });
   }
 
   editProfilePic(link) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    this._request(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         avatar:link,
       })    
-    })
-    .then((res)=> {
-      return this._proccessResponse(res);
-    })  
-    .catch((err) => {
-      console.error(err);
     });
-
   }
-
-  // getId() {
-  // }
   
   deleteCard(cardId) {
-    console.log("cardId: ",cardId);
-       return fetch(`${this._baseUrl}/cards/${cardId}`, {
-          method: "DELETE",
-          headers: this._headers
-        })
-        .then((res)=> {
-          return this._proccessResponse(res);
-        })  
-        .catch((err) => {
-          console.error(err);
-        });
+    this._request(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers
+    });
       }
 
 
   toggleCardLike = (cardId,isLiked) => {
     if (isLiked) {
-      fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-      method: "PUT",
-      headers: this._headers,
-    })
-    .then((res)=> {
-      return this._proccessResponse(res);
-    })  
-    .catch((err) => {
-      console.error(err);
-    });
+      this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: "PUT",
+        headers: this._headers,
+      });
   } else if (!isLiked) {
-    fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+    this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
       method: "DELETE",
       headers: this._headers,
-    })
-    .then((res)=> {
-      return this._proccessResponse(res);
-    })  
-    .catch((err) => {
-      console.error(err);
     });
     }
   };
